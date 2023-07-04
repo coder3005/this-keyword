48ddc9af00705520d35c256a76d6d76a29f05d6a
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-04 22:15:18 +0530

    Create this.java

---
+++ this.java
@@ -0,0 +1,13 @@
+/*this keyword*/
+class A 
+{
+    void show()
+    {
+        System.out.println(this);
+    }
+    public static void main(String[] args) {
+        A r=new A();
+        System.out.println(r);
+        r.show();
+    }
+}
